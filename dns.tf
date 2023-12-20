569e5d9764b640f7eaa8ed6a1497a166a055fd6c
Sharathi25 <149685136+Sharathi25@users.noreply.github.com>
2023-12-21 03:06:19 +0530

    Add files via upload

---
+++ dns.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_dns_zone" "example" {
+  name                = "mydomain.com"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_dns_a_record" "example" {
+  name                = "test"
+  zone_name           = azurerm_dns_zone.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  ttl                 = 300
+  records             = ["10.0.180.17"]
+}
--- sample.tf
+++ sample.tf
@@ -1,16 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
+resource "azurerm_resource_group" "example1" {
+  name     = "example"
+  location = "West Europe"
 }
-provider "azurerm" {
-  features {}
 
-  client_id       = "37ca0f99-5f85-4eb3-897a-c9360a312e66"
-  client_secret   = "a5da7603-fee9-4c3c-97ab-c27c27fd0005"
-  tenant_id       = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
-  subscription_id = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
-}
